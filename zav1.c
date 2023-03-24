3d291b7d0b27a73a37009595d323951b9358126f
lpite <farionsasa@gmail.com>
2023-03-24 19:44:29 +0200

    zav1

--- zav1.c
+++ zav1.c
@@ -1,166 +1,53 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h>
 #include <time.h>
 
-void file_as_file();
-void file_as_terminal();
-void file_as_printer();
+void createFileWithArray() {
+    srand(time(0));
+    FILE* file;
+    file = fopen("j1.txt", "w");
 
-void file_readText(char path[]);
-void file_readBin(char path[]);
+    for (int i = 0; i < 6; i++) {
+        for (int i2 = 0; i2 < 9; i2++) {
+            int randNum = rand() % 10;
+            fprintf(file, "%d", randNum);
+        }
+        fprintf(file, "\n");
+    }
+    fclose(file);
+}
 
-void file_writeText(char path[]);
-void file_writeBin(char path[]);
+void readArrayAndSum() {
+    FILE* file;
+    char a[6][10];
+    int sum = 0, r = 1;
+    file = fopen("j1.txt", "a+");
+    fread(a, sizeof(a), 6, file);
+    for (int i = 0; i < 6; i++) {
+        for (int i2 = 0; i2 < 9; i2++) {
+            printf("%c ", a[i][i2]);
+            if (i == i2) {
+                r = 0;
+            }
+            if (r == 1) {
+                sum += a[i][i2]-'0';
+            }
+        }
+        r = 1;
+        puts("");
+    }
+
+    fprintf(file, "sum = %d\n",sum);
+    fclose(file);
+}
+
+  
 
 int main() {
-	srand(time(0));
-	int arr[6][9],sum = 0;
-	int r = 1,index=0;
-	FILE* file;
-	char str[1000] = "";
 
-
-	for (int i = 0; i < 6; i++)
-	{
-		for (int i2 = 0; i2 < 9; i2++)
-		{
-			int randNum = rand() % 10;
-			str[index] = i2+49;
-			index++;
-			str[index] = ' ';
-			index++;
-			if (i == i2)
-			{
-				r = 0;
-			}
-			if (r == 1)
-			{
-				sum += arr[i][i2];
-
-			}
-		}
-		str[index] = '\n';
-		index++;
-		r = 1;
-	}
-
-	file = fopen("j1.txt", "w");
-	fprintf(file, str);
-	fclose(file);
-
-	int ba[6][9];
-	file = fopen("j1.txt","r");
-	fread(ba,sizeof(arr[0]),6,file);
-	for  (int i  = 0; i  < 6; i++)
-	{
-		for (int i2 = 0; i2 < 9; i2++)
-		{
-			printf("%d\t",ba[i][i2]);
-		}
-		r = 1;
-		puts("");
-	}
-	
-	fclose(file);
-	printf("sum = %d",sum);
-	return 0;
-}
-
-
-void file_as_file()
-{
-	FILE* fp;
-
-	fp = fopen("file.txt", "w");
-	fprintf(fp, "Це текст, який буде записаний у файл\n");
-	fclose(fp);
-}
-void file_as_terminal()
-{
-	printf("Введіть ваше ім'я: ");
-
-	char name[50];
-	scanf("%s", name);
-
-	printf("Привіт, %s!\n", name);
-}
-void file_as_printer()
-{
-	FILE* fp;
-
-	fp = fopen("/dev/lp0", "w");
-	fprintf(fp, "Текст, який буде роздрукований на принтері\n");
-	fclose(fp);
-}
-
-void file_readText(char path[])
-{
-	printf("read\n");
-	FILE* fp;
-	char str[100];
-
-	fp = fopen(path, "r");
-	if (fp == NULL) {
-		printf("Open error ");
-		return;
-	}
-
-	while (fgets(str, 100, fp) != NULL) {
-		printf("%s", str);
-	}
-
-	fclose(fp);
-}
-
-void file_writeText(char path[])
-{
-	printf("write, %s \n", path);
-	FILE* fp;
-
-	fp = fopen(path, "w");
-	if (fp == NULL) {
-		printf("Open error");
-		return;
-	}
-
-	fprintf(fp, "New text to file\n");
-	fclose(fp);
-}
-void file_readBin(char path[])
-{
-	printf("read, %s \n", path);
-
-	FILE* fp;
-	int num;
-
-	//fp = fopen("file.bin", "rb");
-	fp = fopen(path, "rb");
-
-	if (fp == NULL) {
-		printf("Open error");
-		return -1;
-	}
-
-	while (fread(&num, sizeof(int), 1, fp) == 1) {
-		printf("%d ", num);
-	}
-
-	fclose(fp);
-}
-void file_writeBin(char path[])
-{
-	FILE* fp;
-	int nums[] = { 1, 2, 3, 4, 5 };
-
-	fp = fopen(path, "wb");
-	if (fp == NULL) {
-		printf("Open Error");
-		return -1;
-	}
-
-	fwrite(nums, sizeof(int), 5, fp);
-
-	fclose(fp);
+    createFileWithArray();
+    readArrayAndSum();
+   
+    return 0;
 }
